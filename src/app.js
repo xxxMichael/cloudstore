e85b2c838aa2734ff994efd167dcb933fc5f1b0c
PAUL-23VE <paulvelastegui2016@gmail.com>
2025-11-18 19:41:05 -0500

    se arreglo los permisos de cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use(cors());
 
 // Rutas
 app.use("/", routes);
